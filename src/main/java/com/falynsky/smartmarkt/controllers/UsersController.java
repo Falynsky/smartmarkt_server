7b2f720086db9fe1a9fb071c466ea967ccdaeccf
Falynsky <kamil.falinski@gmail.com>
2020-04-12 23:43:54 +0200

    Przeniesienie bazy z MySQL na MS SQL Server + dodanie obsługi Liquibase

--- src/main/java/com/falynsky/smartmarkt/controllers/UsersController.java
+++ src/main/java/com/falynsky/smartmarkt/controllers/UsersController.java
@@ -1,16 +1,13 @@
 package com.falynsky.smartmarkt.controllers;
 
-import com.falynsky.smartmarkt.models.DTO.Users;
-import com.falynsky.smartmarkt.models.UsersEntity;
+import com.falynsky.smartmarkt.models.DTO.UsersDTO;
+import com.falynsky.smartmarkt.models.Users;
 import com.falynsky.smartmarkt.repositories.UsersRepository;
-import com.fasterxml.jackson.core.JsonFactoryBuilder;
 import com.google.gson.Gson;
-import org.apache.catalina.User;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
@@ -26,25 +23,25 @@ public class UsersController {
     }
 
     @GetMapping("/all")
-    public List<Users> getAllUsers() {
+    public List<UsersDTO> getAllUsers() {
         return usersRepository.retrieveUsersAsDTO();
     }
 
     @GetMapping("/{id}")
-    public Users getAllUsers(@PathVariable("id") Integer id) {
+    public UsersDTO getAllUsers(@PathVariable("id") Integer id) {
         return usersRepository.retrieveUsersAsDTObyId(id);
     }
 
     @RequestMapping("/create")
     public void addNewUser() {
-        Users user = new Users(4, "test4", "test");
-        UsersEntity usersEntity = new UsersEntity(user);
+        UsersDTO user = new UsersDTO(4, "test4", "test");
+        Users usersEntity = new Users(user);
         usersRepository.save(usersEntity);
     }
 
     @DeleteMapping("/{userId}")
     public String deleteUser(@PathVariable Integer userId) {
-        Optional<UsersEntity> userToDelete = usersRepository.findById(userId);
+        Optional<Users> userToDelete = usersRepository.findById(userId);
         if (userToDelete.isPresent()) {
             usersRepository.delete(userToDelete.get());
             return "User deleted with id = " + userId;
@@ -54,24 +51,24 @@ public class UsersController {
     }
 
     @GetMapping("/mocked")
-    public Users getMockedUser() {
-        Users mockedUser = new Users(1, "mockedUser", "mockedUser");
+    public UsersDTO getMockedUser() {
+        UsersDTO mockedUser = new UsersDTO(1, "mockedUser", "mockedUser");
         Gson gson = new Gson();
         String json = gson.toJson(mockedUser);
         return mockedUser;
     }
 
     @GetMapping("/mocked/{numberOfUsers}")
-    public List<Users> getAllMockedUsers(@PathVariable Integer numberOfUsers) {
-        List<Users> mockedUsers =
+    public List<UsersDTO> getAllMockedUsers(@PathVariable Integer numberOfUsers) {
+        List<UsersDTO> mockedUsers =
                 generateUsers(numberOfUsers);
         return mockedUsers;
     }
 
-    private List<Users> generateUsers(int numberOfUsers) {
-        List<Users> users = new ArrayList<>();
+    private List<UsersDTO> generateUsers(int numberOfUsers) {
+        List<UsersDTO> users = new ArrayList<>();
         for (int i = 1; i <= numberOfUsers; i++) {
-            users.add(new Users(i, "user" + i, "user" + i));
+            users.add(new UsersDTO(i, "user" + i, "user" + i));
         }
         return users;
     }
--- src/main/java/com/falynsky/smartmarkt/repositories/UsersRepository.java
+++ src/main/java/com/falynsky/smartmarkt/repositories/UsersRepository.java
@@ -1,23 +1,22 @@
 package com.falynsky.smartmarkt.repositories;
 
-import com.falynsky.smartmarkt.models.DTO.Users;
-import com.falynsky.smartmarkt.models.UsersEntity;
+import com.falynsky.smartmarkt.models.DTO.UsersDTO;
+import com.falynsky.smartmarkt.models.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
-public interface UsersRepository extends JpaRepository<UsersEntity, Integer> {
+public interface UsersRepository extends JpaRepository<Users, Integer> {
 
-    @Query("SELECT new com.falynsky.smartmarkt.models.DTO.Users(u.id, u.login, u.password) FROM UsersEntity u")
-    List<Users> retrieveUsersAsDTO();
+    @Query("SELECT new com.falynsky.smartmarkt.models.DTO.UsersDTO(u.id, u.login, u.password) FROM Users u")
+    List<UsersDTO> retrieveUsersAsDTO();
 
-    @Query("SELECT new com.falynsky.smartmarkt.models.DTO.Users(u.id, u.login, u.password) FROM UsersEntity u where u.id = :userId")
-    Users retrieveUsersAsDTObyId(@Param("userId") Integer userId);
+    @Query("SELECT new com.falynsky.smartmarkt.models.DTO.UsersDTO(u.id, u.login, u.password) FROM Users u where u.id = :userId")
+    UsersDTO retrieveUsersAsDTObyId(@Param("userId") Integer userId);
 
 
 }
