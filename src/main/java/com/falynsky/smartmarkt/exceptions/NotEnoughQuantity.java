20a7c58f7fd970db750369546ebe0b7295aeb8d8
Kamil Faliński <kamil.falinski@gmail.com>
2021-01-27 21:46:15 +0100

    Przed ostatnie poprawki po konsultacji z promotorem

--- src/main/java/com/falynsky/smartmarkt/exceptions/NotEnoughQuantity.java
+++ src/main/java/com/falynsky/smartmarkt/exceptions/NotEnoughQuantity.java
@@ -3,18 +3,17 @@ package com.falynsky.smartmarkt.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR, reason = "Nie wystarczająca liczba produktów")
+@ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR, reason = "Niewystarczająca liczba produktów")
 public class NotEnoughQuantity extends Exception {
 
-    private static final String EXCEPTION_MESSAGE = "Nie wystarczająca liczba produktów";
-    private static final String EXCEPTION_MESSAGE_WITH_RPODUCT_NAME = "Nie wystarczająca liczba produktów - ";
+    private static final String EXCEPTION_MESSAGE = "Niewystarczająca liczba produktów";
 
     public NotEnoughQuantity() {
         super(EXCEPTION_MESSAGE);
     }
 
     public NotEnoughQuantity(String name) {
-        super((EXCEPTION_MESSAGE_WITH_RPODUCT_NAME+name).toString());
+        super(EXCEPTION_MESSAGE + " - " + name);
     }
 
 }
